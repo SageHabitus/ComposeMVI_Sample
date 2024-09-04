e12c29640fe2672a873e72a06fcf7005cda5f611
sageHabitus <dunkey2020@gmail.com>
2024-09-05 00:02:00 +0900

    👔 update searchBooks api business logic exception

--- app/src/main/java/com/example/composemvi/data/source/remote/source/BookRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/composemvi/data/source/remote/source/BookRemoteDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.example.composemvi.data.source.remote.source
 
 import com.example.composemvi.data.source.remote.api.BookApi
+import com.example.composemvi.data.source.remote.exception.ApiExceptionMapper
 import com.example.composemvi.data.source.remote.model.BookResponseModel
 import javax.inject.Inject
 
@@ -8,7 +9,13 @@ class BookRemoteDataSourceImpl @Inject constructor(
     private val api: BookApi,
 ) : BookRemoteDataSource {
 
-    override suspend fun searchBooks(query: String, page: Int, size: Int): BookResponseModel {
-        return api.searchBooks(query, page, size)
+    override suspend fun searchBooks(
+        query: String,
+        page: Int,
+        size: Int
+    ): BookResponseModel = runCatching {
+        api.searchBooks(query = query, page = page, size = size)
+    }.getOrElse { exception ->
+        throw ApiExceptionMapper.toException(exception)
     }
 }
