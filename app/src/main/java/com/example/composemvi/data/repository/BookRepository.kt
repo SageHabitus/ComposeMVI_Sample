6d16e2f75401eabf2f1fe7ff64a568dce627bbf5
sageHabitus <dunkey2020@gmail.com>
2024-09-06 21:24:38 +0900

    👔 add book repository implementation

---
+++ app/src/main/java/com/example/composemvi/data/repository/BookRepository.kt
@@ -0,0 +1,20 @@
+package com.example.composemvi.data.repository
+
+import androidx.paging.PagingData
+import com.example.composemvi.data.model.BookDataModel
+import kotlinx.coroutines.flow.Flow
+
+interface BookRepository {
+
+    suspend fun searchAndCacheBooks(query: String): Flow<PagingData<BookDataModel>>
+
+    suspend fun updateBookmarkStatus(isbn: String, isBookmarked: Boolean)
+
+    suspend fun getAllBooks(): Flow<PagingData<BookDataModel>>
+
+    suspend fun getAllBookmarkedBooks(): Flow<PagingData<BookDataModel>>
+
+    suspend fun getBookByIsbn(isbn: String): BookDataModel
+
+    suspend fun isBookExistsByIsbn(isbn: String): Boolean
+}
