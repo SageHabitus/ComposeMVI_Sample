f03f641b5355ca59c1dcc8ba1d1cf3e3ed24e9e9
sageHabitus <dunkey2020@gmail.com>
2024-09-09 00:05:22 +0900

    📝 delete logger in ComposeMVIApplication

--- app/src/main/java/com/example/composemvi/core/ComposeMVIApplication.kt
+++ app/src/main/java/com/example/composemvi/core/ComposeMVIApplication.kt
@@ -1,27 +1,7 @@
 package com.example.composemvi.core
 
 import android.app.Application
-import com.orhanobut.logger.AndroidLogAdapter
-import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ComposeMVIApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Logger.addLogAdapter(CustomLogAdapter())
-    }
-}
-
-class CustomLogAdapter : AndroidLogAdapter() {
-    override fun log(priority: Int, tag: String?, message: String) {
-        val customTag =
-            tag ?: run {
-                val stackTrace = Throwable().stackTrace
-                val className = stackTrace[6].className.substringAfterLast('.')
-                className
-            }
-
-        super.log(priority, customTag, message)
-    }
-}
+class ComposeMVIApplication : Application()
